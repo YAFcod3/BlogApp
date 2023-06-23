fad367d8a86aac86b4d9c0b7a3c625bbc8d8c681
YAFcod3 <yasmaniarmas1995@gmail.com>
2023-06-23 15:09:20 -0400

    about and contact ok

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
-import style from './page.module.css'
+import React from "react";
+import styles from "./page.module.css";
+import Image from "next/image";
+import Button from "@/components/button/Button";
 
 function Contact() {
   return (
-    <div className={style.contact}>Contact</div>
-  )
+    <div className={styles.container}>
+      {/* titulo */}
+      <h1 className={styles.title}>Let's Keep in Touch</h1>
+
+      {/* contenido */}
+      <div className={styles.content}>
+        {/* imagen */}
+        <div className={styles.imgContainer}>
+          <Image
+            src="/contact.png"
+            alt=""
+            fill={true}
+            className={styles.image}
+          />
+        </div>
+        {/* formulario */}
+        <form className={styles.form}>
+          <input type="text" placeholder="name" className={styles.input} />
+          <input type="text" placeholder="email" className={styles.input} />
+          <textarea
+            className={styles.textArea}
+            placeholder="message"
+            cols="30"
+            rows="10"
+          ></textarea>
+          <Button url="#" text="Send" />
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default Contact
+export default Contact;
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Hero from "public/hero.png";
+import Button from "@/components/button/Button";
 
 export default function Home() {
   return (
@@ -11,7 +12,7 @@ export default function Home() {
           Turning your idea into Reality. We bring together the teams frm the
           global teach indsutry
         </p>
-        <button className={styles.button}> See Our industry</button>
+        <Button url='/portafolio' text='See Our industry'/>
       </div>
 
       <div className={styles.item}>
