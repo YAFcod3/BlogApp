4f6f6e763570e34459773bb0fc990076a1ce0f5e
YAFcod3 <yasmaniarmas1995@gmail.com>
2023-09-01 13:01:43 -0400

    adding oAuth js

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,10 +1,12 @@
 "use client";
+import { useSession } from "next-auth/react";
 import React from "react";
 import useSWR from "swr";
 
 function Dashboard() {
 
-
+  const session=useSession()
+  console.log(session)
 
   const fetcher = (...args) => fetch(...args).then((res) => res.json());
   const { data, error, isLoading } = useSWR("https://jsonplaceholder.typicode.com/posts",fetcher);
--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,7 @@ import Navbar from "@/components/navbar/Navbar";
 import "./globals.css";
 import { Inter, Roboto, Poppins } from "next/font/google";
 import Footer from "@/components/footer/Footer";
-
+import AuthProvider from "@/components/AuthProvider/AuthProvider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -14,12 +14,15 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <AuthProvider>
+
         <div className="container">
         
           <Navbar />
           {children}
         <Footer />
         </div>
+        </AuthProvider>
        
       </body>
     </html>
